fc5135461714b4d842ab26ec70e98f06a15e0dcc
Daniel de Horta <128613954+BadBoosbyCh@users.noreply.github.com>
2023-05-23 09:06:05 -0600

    Update 23/05/2022

--- Javascript Code -Daniel/js/main.js
+++ Javascript Code -Daniel/js/main.js
@@ -1,2 +1,26 @@
 console.log("Hello World");
-alert("Bonjour le monde")
+//alert("Bonjour le monde")
+/*
+Comentario Multilinea
+
+
+let nombre= prompt("¿Cual es tu nombre?");
+cosnole.log(nombre);
+
+console.log(typeof(nombre));
+*/
+ 
+let numeroUno = prompt("Ingresa un numero");
+
+let numeroDos = prompt("Ingresa el otro numero");
+
+
+let resultadoSuma = parseInt(numeroUno) + parseInt(numeroDos);
+let resultadoResta = parseInt(numeroUno) - parseInt(numeroDos);
+let resultadoDivision = parseInt(numeroUno) / parseInt(numeroDos);
+let resultadoMultiplicacion = parseInt(numeroUno) * parseInt(numeroDos);
+
+console.log(`El resultado de la suma es: ${resultadoSuma}`);
+console.log(`El resultado de la resta es: ${resultadoResta}`);
+console.log(`El resultado de la division es: ${resultadoDivision}`);
+console.log(`El resultado de la multiplicacion es: ${resultadoMultiplicacion}`);
