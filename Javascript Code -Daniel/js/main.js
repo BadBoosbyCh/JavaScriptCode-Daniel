3168e4696e0d94fc35a447c20deccd54e23dee43
Daniel de Horta <128613954+BadBoosbyCh@users.noreply.github.com>
2023-06-02 09:59:09 -0600

    Update 2/6/2023
    ajksdajxd

--- Javascript Code -Daniel/js/main.js
+++ Javascript Code -Daniel/js/main.js
@@ -8,7 +8,7 @@ let nombre= prompt("¿Cual es tu nombre?");
 cosnole.log(nombre);
 
 console.log(typeof(nombre));
-*/
+
  
 let numeroUno = prompt("Ingresa un numero");
 
@@ -24,3 +24,25 @@ console.log(`El resultado de la suma es: ${resultadoSuma}`);
 console.log(`El resultado de la resta es: ${resultadoResta}`);
 console.log(`El resultado de la division es: ${resultadoDivision}`);
 console.log(`El resultado de la multiplicacion es: ${resultadoMultiplicacion}`);
+
+
+let numeroUno = prompt("Ingresa un numero ");
+let numeroDos = prompt ("Ingresa otro numero");
+
+if (numeroUno > numeroDos){
+console.log(`El numero ${numeroUno}esa mayor que ${numeroDos}`);
+}else if( numeroDos>numeroUno){
+    console.log(`El numero ${numeroDos} es mayor que ${numeroUno} `);
+}else{
+    console.log(`El numero ${numeroUno} es igual a ${numeroDos}`);
+
+} 
+*/
+let numeroUno = prompt("Ingresa un numero ");
+if(numeroUno %2==0){
+    console.log(`El numero: ${numeroUno}par `);
+
+}
+else{
+    console.log(`El numero: ${numerDos}impar `);
+}
